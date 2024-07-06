b15df2ad3510a20aef5f28245c0b930d10717aa5
JangWoojun <woojun0107@naver.com>
2024-07-07 00:58:28 +0900

    refactor: 패키지 위치 변경

--- app/src/main/java/com/woojun/shock/view/MainActivity.kt
+++ app/src/main/java/com/woojun/shock/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.woojun.shock
+package com.woojun.shock.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.woojun.shock.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
