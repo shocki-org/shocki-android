93c698ed135d04ac2d4a958848e888bd881e82f5
JangWoojun <woojun0107@naver.com>
2024-08-15 09:19:36 +0900

    feat: AuthActivity 추가

---
+++ app/src/main/java/com/woojun/shocki/view/auth/AuthActivity.kt
@@ -0,0 +1,14 @@
+package com.woojun.shocki.view.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.woojun.shocki.databinding.ActivityAuthBinding
+
+class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
