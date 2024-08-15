898bb559ff1260edc09c9e797edae2248ecbfe3b
JangWoojun <woojun0107@naver.com>
2024-08-15 09:53:16 +0900

    feat: SystemBar 고려한 패딩 추가

--- app/src/main/java/com/woojun/shocki/view/MainActivity.kt
+++ app/src/main/java/com/woojun/shocki/view/MainActivity.kt
@@ -3,8 +3,11 @@ package com.woojun.shocki.view
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.woojun.shocki.R
@@ -19,6 +22,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         navController = findNavController(R.id.nav_host_fragment)
 
