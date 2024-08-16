f13944dd64a2abf1a76d732f33cc9e3b7ebfd6c3
JangWoojun <woojun0107@naver.com>
2024-08-16 11:13:09 +0900

    feat: 스테이터스 바 투명화 구현
    투명화 및 패딩 설정

--- app/src/main/java/com/woojun/shocki/view/nav/ExploreFragment.kt
+++ app/src/main/java/com/woojun/shocki/view/nav/ExploreFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.SystemBarStyle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.woojun.shocki.databinding.FragmentExploreBinding
 
 class ExploreFragment : Fragment() {
@@ -26,6 +30,21 @@ class ExploreFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupWindowInsets()
+    }
+
+    private fun setupWindowInsets() {
+        requireActivity().enableEdgeToEdge(SystemBarStyle.dark(0xffffff))
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val statusBars = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+
+            val statusBarHeight = statusBars.top
+            val navBarHeight = systemBars.bottom
+
+            binding.buttonLayout.setPadding(0, statusBarHeight, 0, navBarHeight)
+            insets
+        }
     }
 
     override fun onDestroy() {
