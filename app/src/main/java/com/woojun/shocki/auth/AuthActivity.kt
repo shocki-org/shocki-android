c8c3950ad98270394a9d9c49c8afbba3a50fa409
JangWoojun <woojun0107@naver.com>
2024-08-19 14:57:36 +0900

    feat: navController 연결

--- app/src/main/java/com/woojun/shocki/auth/AuthActivity.kt
+++ app/src/main/java/com/woojun/shocki/auth/AuthActivity.kt
@@ -2,13 +2,18 @@ package com.woojun.shocki.auth
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.woojun.shocki.R
 import com.woojun.shocki.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAuthBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navController = findNavController(R.id.nav_host_fragment)
     }
 }
