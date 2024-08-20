6bcdc91d51a24951652c76b7f36114a53ecf1290
JangWoojun <woojun0107@naver.com>
2024-08-20 17:13:28 +0900

    feat: SplashActivity 세팅
    SplashAPI 제거 및 첫 화면으로 설정, 2초 뒤 넘기기

--- app/src/main/java/com/woojun/shocki/view/main/SplashActivity.kt
+++ app/src/main/java/com/woojun/shocki/view/main/SplashActivity.kt
@@ -1,16 +1,28 @@
 package com.woojun.shocki.view.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.woojun.shocki.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Thread.sleep
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            sleep(2000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finishAffinity()
+        }
     }
 }
