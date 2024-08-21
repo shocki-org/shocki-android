ab023f3cbc1f092ea9ad4483abc4aa9825c49fcb
JangWoojun <woojun0107@naver.com>
2024-08-21 15:54:22 +0900

    feat: SearchFragment 추가

---
+++ app/src/main/java/com/woojun/shocki/view/nav/search/SearchFragment.kt
@@ -0,0 +1,36 @@
+package com.woojun.shocki.view.nav.search
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woojun.shocki.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private var _binding: FragmentSearchBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
