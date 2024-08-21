4a4d79b211c6611f62586ea8ab64a2e73bc576bd
JangWoojun <woojun0107@naver.com>
2024-08-21 14:37:43 +0900

    refactor: 필요없는 코드 제거

--- app/src/main/java/com/woojun/shocki/view/main/SplashActivity.kt
+++ app/src/main/java/com/woojun/shocki/view/main/SplashActivity.kt
@@ -2,11 +2,8 @@ package com.woojun.shocki.view.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.woojun.shocki.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
