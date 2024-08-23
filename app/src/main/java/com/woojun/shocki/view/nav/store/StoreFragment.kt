6617247ee5b0c2727044e101195fdd76693add15
JangWoojun <woojun0107@naver.com>
2024-08-23 18:54:43 +0900

    feat: StoreFragment 버튼 action 및 RecyclerView 연결

--- app/src/main/java/com/woojun/shocki/view/nav/store/StoreFragment.kt
+++ app/src/main/java/com/woojun/shocki/view/nav/store/StoreFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.woojun.shocki.R
+import com.woojun.shocki.data.Banner
 import com.woojun.shocki.databinding.FragmentStoreBinding
+import com.woojun.shocki.view.main.MainActivity
 
 class StoreFragment : Fragment() {
 
@@ -26,6 +30,36 @@ class StoreFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val activity = (requireActivity() as MainActivity)
+
+        binding.shippingButton.setOnClickListener {
+            activity.animationNavigate(R.id.notification)
+        }
+
+        binding.searchButton.setOnClickListener {
+            activity.animationNavigate(R.id.search)
+        }
+
+        binding.storeList.apply {
+            this.layoutManager = GridLayoutManager(requireContext(), 2)
+            this.adapter = StoreAdapter(getTestBannerList())
+        }
+
+    }
+
+    private fun getTestBannerList(): List<Banner> {
+        val text = "정성담아 키워낸, 해남 황토 꿀고구마정성담아 키워낸, 해남 황토 꿀고구마"
+        return listOf(
+            Banner(R.drawable.banner6, text, "1,232,145"),
+            Banner(R.drawable.banner1, text, "1,232,145"),
+            Banner(R.drawable.banner4, text, "1,232,145"),
+            Banner(R.drawable.banner3, text, "1,232,145"),
+            Banner(R.drawable.banner6, text, "1,232,145"),
+            Banner(R.drawable.banner1, text, "1,232,145"),
+            Banner(R.drawable.banner4, text, "1,232,145"),
+            Banner(R.drawable.banner3, text, "1,232,145"),
+        )
     }
 
     override fun onDestroy() {
