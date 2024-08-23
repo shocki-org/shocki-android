04c9ead0e71259e278e8fe041e7c77ba662bd4d9
JangWoojun <woojun0107@naver.com>
2024-08-23 18:54:11 +0900

    feat: StoreAdapter 추가

---
+++ app/src/main/java/com/woojun/shocki/view/nav/store/StoreAdapter.kt
@@ -0,0 +1,32 @@
+package com.woojun.shocki.view.nav.store
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.woojun.shocki.data.Banner
+import com.woojun.shocki.databinding.StoreItemBinding
+
+class StoreAdapter (private val storeList: List<Banner>):
+    RecyclerView.Adapter<StoreAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoreAdapter.ViewHolder {
+        val binding = StoreItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding).also { handler ->
+
+        }
+    }
+
+    override fun onBindViewHolder(holder: StoreAdapter.ViewHolder, position: Int) {
+        holder.bind(storeList[position])
+    }
+
+    override fun getItemCount(): Int = storeList.size
+
+    inner class ViewHolder(private val binding : StoreItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(item: Banner) {
+            binding.nameText.text = item.text
+            binding.priceText.text = item.price
+            binding.imageView.setImageResource(item.image)
+        }
+    }
+}
