469c980d13b9e43243d5302c968c31fff760042e
JangWoojun <woojun0107@naver.com>
2024-08-24 15:17:55 +0900

    feat: ShippingAdapter 연결

--- app/src/main/java/com/woojun/shocki/view/nav/shipping/ShippingFragment.kt
+++ app/src/main/java/com/woojun/shocki/view/nav/shipping/ShippingFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.woojun.shocki.R
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.woojun.shocki.databinding.FragmentShippingBinding
 
 class ShippingFragment : Fragment() {
+
+    private var _binding: FragmentShippingBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +21,37 @@ class ShippingFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_shipping, container, false)
+    ): View {
+        _binding = FragmentShippingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.shippingList.apply {
+            this.layoutManager = LinearLayoutManager(requireContext())
+            this.adapter = ShippingAdapter(testList())
+        }
+
+
+    }
+
+    private fun testList(): List<String> {
+        return listOf(
+            "활용법까지 알려주는 디자이너 PICK <무료 폰트 가이드>",
+            "활용법까지 알려주는 디자이너 PICK <무료 폰트 가이드>",
+            "활용법까지 알려주는 디자이너 PICK <무료 폰트 가이드>",
+            "활용법까지 알려주는 디자이너 PICK <무료 폰트 가이드>"
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
