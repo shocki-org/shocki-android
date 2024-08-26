7b7f932934347226b5e97b0431249cb5aafb48cd
JangWoojun <woojun0107@naver.com>
2024-08-26 15:12:56 +0900

    feat: back_button 구현

--- app/src/main/java/com/woojun/shocki/view/nav/detail/FundingDetailFragment.kt
+++ app/src/main/java/com/woojun/shocki/view/nav/detail/FundingDetailFragment.kt
@@ -1,12 +1,18 @@
 package com.woojun.shocki.view.nav.detail
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsetsController
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.tabs.TabLayout
 import com.woojun.shocki.R
@@ -30,9 +36,14 @@ class FundingDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupWindowInsets()
 
         val adapter1 = ImageAdapter(listOf(R.drawable.banner1, R.drawable.banner2, R.drawable.banner3, R.drawable.banner4, R.drawable.banner5))
         val adapter2 = ChatAdapter(listOf())
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
 
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(requireContext())
@@ -79,6 +90,33 @@ class FundingDetailFragment : Fragment() {
 
 
     }
+
+    private fun setupWindowInsets() {
+        requireActivity().enableEdgeToEdge()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            requireActivity().window.insetsController?.setSystemBarsAppearance(
+                0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            requireActivity().window.apply {
+                this.decorView.systemUiVisibility = this.decorView.systemUiVisibility and
+                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val statusBars = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+
+            val statusBarHeight = statusBars.top
+            val navBarHeight = systemBars.bottom
+
+            binding.backButton.setPadding(0, statusBarHeight, 0, navBarHeight)
+            insets
+        }
+    }
+
 
     private fun createTabView(tabName: String): View {
         val tabView = LayoutInflater.from(requireContext()).inflate(R.layout.custom_tab, null)
--- app/src/main/java/com/woojun/shocki/view/nav/detail/StoreDetailFragment.kt
+++ app/src/main/java/com/woojun/shocki/view/nav/detail/StoreDetailFragment.kt
@@ -1,10 +1,16 @@
 package com.woojun.shocki.view.nav.detail
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowInsetsController
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.woojun.shocki.R
 import com.woojun.shocki.databinding.FragmentStoreDetailBinding
@@ -28,6 +34,11 @@ class StoreDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupWindowInsets()
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
 
         binding.imageList.apply {
             this.adapter = ImageAdapter(
@@ -40,6 +51,33 @@ class StoreDetailFragment : Fragment() {
 
         }
     }
+
+    private fun setupWindowInsets() {
+        requireActivity().enableEdgeToEdge()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            requireActivity().window.insetsController?.setSystemBarsAppearance(
+                0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        } else {
+            requireActivity().window.apply {
+                this.decorView.systemUiVisibility = this.decorView.systemUiVisibility and
+                        View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val statusBars = insets.getInsets(WindowInsetsCompat.Type.statusBars())
+
+            val statusBarHeight = statusBars.top
+            val navBarHeight = systemBars.bottom
+
+            binding.backButton.setPadding(0, statusBarHeight, 0, navBarHeight)
+            insets
+        }
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
