8103048f4ea8268e5d8718940fb9ba721a815838
JangWoojun <woojun0107@naver.com>
2024-08-31 21:55:01 +0900

    feat: QnaFragment 추가

---
+++ app/src/main/java/com/woojun/shocki/view/nav/detail/QnaFragment.kt
@@ -0,0 +1,37 @@
+package com.woojun.shocki.view.nav.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woojun.shocki.databinding.FragmentQnaBinding
+
+class QnaFragment : Fragment() {
+
+    private var _binding: FragmentQnaBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentQnaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
