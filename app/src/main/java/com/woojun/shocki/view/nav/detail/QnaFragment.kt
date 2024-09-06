81244741d7afab9ccfa16b655b35e690223406d9
JangWoojun <woojun0107@naver.com>
2024-09-06 22:50:31 +0900

    feat: backButton action 추가

--- app/src/main/java/com/woojun/shocki/view/nav/detail/QnaFragment.kt
+++ app/src/main/java/com/woojun/shocki/view/nav/detail/QnaFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.woojun.shocki.databinding.FragmentQnaBinding
 
 class QnaFragment : Fragment() {
@@ -27,6 +28,11 @@ class QnaFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
     }
 
     override fun onDestroy() {
