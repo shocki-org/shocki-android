f7ca7acfe20fc6fabfc98d5c98744477687d1d62
JangWoojun <woojun0107@naver.com>
2024-09-15 15:29:15 +0900

    feat: TokenManager 추가
    ACCESS_TOKEN을 관리할 SharedPreferences 추가

---
+++ app/src/main/java/com/woojun/shocki/database/TokenManager.kt
@@ -0,0 +1,29 @@
+package com.woojun.shocki.database
+
+import android.content.Context
+import android.content.SharedPreferences
+
+object TokenManager {
+    private const val NAME = "Shocki"
+    private const val MODE = Context.MODE_PRIVATE
+    private lateinit var preferences: SharedPreferences
+
+    private val ACCESS_TOKEN = Pair("accessToken", "")
+
+
+    fun init(context: Context) {
+        preferences = context.getSharedPreferences(NAME, MODE)
+    }
+
+    private inline fun SharedPreferences.edit(operation: (SharedPreferences.Editor) -> Unit) {
+        val editor = edit()
+        operation(editor)
+        editor.apply()
+    }
+
+    var accessToken: String
+        get() = preferences.getString(ACCESS_TOKEN.first, ACCESS_TOKEN.second) ?: ""
+        set(value) = preferences.edit {
+            it.putString(ACCESS_TOKEN.first, value)
+        }
+}
