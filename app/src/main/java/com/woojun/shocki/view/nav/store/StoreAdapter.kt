160ccac4c7be932ec4aa5da5c15317bc0a666022
JangWoojun <woojun0107@naver.com>
2024-09-18 15:05:39 +0900

    feat: 세부 페이지 이동 및 바뀐 데이터 binding

--- app/src/main/java/com/woojun/shocki/view/nav/store/StoreAdapter.kt
+++ app/src/main/java/com/woojun/shocki/view/nav/store/StoreAdapter.kt
@@ -3,19 +3,20 @@ package com.woojun.shocki.view.nav.store
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.woojun.shocki.R
-import com.woojun.shocki.data.Banner
 import com.woojun.shocki.databinding.StoreItemBinding
+import com.woojun.shocki.dto.SimpleProductResponse
 import com.woojun.shocki.view.main.MainActivity
 
-class StoreAdapter (private val storeList: List<Banner>):
+class StoreAdapter (private val storeList: List<SimpleProductResponse>):
     RecyclerView.Adapter<StoreAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoreAdapter.ViewHolder {
         val binding = StoreItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return ViewHolder(binding).also { handler ->
             binding.root.setOnClickListener {
-                (binding.root.context as MainActivity).animationNavigate(R.id.storeDetail)
+                (binding.root.context as MainActivity).animationNavigate(R.id.storeDetail, storeList[handler.adapterPosition].id)
             }
         }
     }
@@ -27,10 +28,14 @@ class StoreAdapter (private val storeList: List<Banner>):
     override fun getItemCount(): Int = storeList.size
 
     inner class ViewHolder(private val binding : StoreItemBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(item: Banner) {
-            binding.nameText.text = item.text
-            binding.priceText.text = item.price
-            binding.imageView.setImageResource(item.image)
+        fun bind(item: SimpleProductResponse) {
+            binding.nameText.text = item.name
+            binding.priceText.text = item.currentAmount.toString()
+            Glide
+                .with(binding.root.context)
+                .load(item.image)
+                .centerCrop()
+                .into(binding.imageView)
         }
     }
 }
