f7d341978c5cd0f86eef3dccfc0f87df7eccbddd
JangWoojun <woojun0107@naver.com>
2024-09-22 20:58:16 +0900

    feat: 완료 액션 구현

--- app/src/main/java/com/woojun/shocki/view/auth/WalletFinishFragment.kt
+++ app/src/main/java/com/woojun/shocki/view/auth/WalletFinishFragment.kt
@@ -1,11 +1,14 @@
 package com.woojun.shocki.view.auth
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.woojun.shocki.R
 import com.woojun.shocki.databinding.FragmentWalletFinishBinding
+import com.woojun.shocki.view.main.MainActivity
 
 class WalletFinishFragment : Fragment() {
 
@@ -26,6 +29,11 @@ class WalletFinishFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.mainButton.setOnClickListener {
+            startActivity(Intent(requireActivity(), MainActivity::class.java))
+            requireActivity().finishAffinity()
+        }
     }
 
     override fun onDestroyView() {
