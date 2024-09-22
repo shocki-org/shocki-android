0c3baeef319f3d8b3a439aef0f46b9309e16c7ac
JangWoojun <woojun0107@naver.com>
2024-09-22 20:01:45 +0900

    feat: WalletFinishFragment, ConnectWalletFragment 추가

---
+++ app/src/main/java/com/woojun/shocki/view/auth/ConnectWalletFragment.kt
@@ -0,0 +1,37 @@
+package com.woojun.shocki.view.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woojun.shocki.databinding.FragmentConnectWalletBinding
+
+
+class ConnectWalletFragment : Fragment() {
+
+    private var _binding: FragmentConnectWalletBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentConnectWalletBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
---
+++ app/src/main/java/com/woojun/shocki/view/auth/WalletFinishFragment.kt
@@ -0,0 +1,36 @@
+package com.woojun.shocki.view.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.woojun.shocki.databinding.FragmentWalletFinishBinding
+
+class WalletFinishFragment : Fragment() {
+
+    private var _binding: FragmentWalletFinishBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWalletFinishBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
