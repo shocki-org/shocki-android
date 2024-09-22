8a0f4d99dac6f52ea8a234da467037a60d43b43e
JangWoojun <woojun0107@naver.com>
2024-09-22 20:57:59 +0900

    feat: 메타마스크 지갑 연결 로직 구현 및 뒤로가기 구현

--- app/src/main/java/com/woojun/shocki/view/auth/ConnectWalletFragment.kt
+++ app/src/main/java/com/woojun/shocki/view/auth/ConnectWalletFragment.kt
@@ -5,7 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.woojun.shocki.R
+import com.woojun.shocki.database.TokenManager
 import com.woojun.shocki.databinding.FragmentConnectWalletBinding
+import com.woojun.shocki.dto.WalletRequest
+import com.woojun.shocki.network.RetrofitAPI
+import com.woojun.shocki.network.RetrofitClient
+import com.woojun.shocki.view.model.MetaMaskModel
+import io.metamask.androidsdk.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class ConnectWalletFragment : Fragment() {
@@ -27,6 +40,40 @@ class ConnectWalletFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+
+        binding.mainButton.setOnClickListener {
+            when (val result = MetaMaskModel.connectToEthereum(requireContext())) {
+                is Result.Success.Items -> {
+                    lifecycleScope.launch {
+                        val isSuccess = setWallet(result.value[0])
+                        if (isSuccess) {
+                            (activity as AuthActivity).animationNavigate(R.id.walletFinish)
+                        } else {
+                            Toast.makeText(requireContext(), "메타마스크 지갑 연결을 실패했어요", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                else -> {
+                    Toast.makeText(requireContext(), "메타마스크 지갑 연결을 실패했어요", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private suspend fun setWallet(address: String): Boolean {
+        return try {
+            withContext(Dispatchers.IO) {
+                val retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI::class.java)
+                val response = retrofitAPI.setWallet("bearer ${TokenManager.accessToken}", WalletRequest(address))
+                response.isSuccessful
+            }
+        } catch (e: Exception) {
+            false
+        }
     }
 
     override fun onDestroyView() {
