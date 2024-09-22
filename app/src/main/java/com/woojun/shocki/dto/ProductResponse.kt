606b3e832a14a9bf73ba4dbea13f376e50c9ed77
JangWoojun <woojun0107@naver.com>
2024-09-22 23:58:10 +0900

    feat: ProductResponse purchaseIsDisabled 속성 추가

--- app/src/main/java/com/woojun/shocki/dto/ProductResponse.kt
+++ app/src/main/java/com/woojun/shocki/dto/ProductResponse.kt
@@ -9,17 +9,19 @@ data class ProductResponse(
     val createdAt: String,
     val currentAmount: Int,
     val detailImages: List<String>,
-    val distributionPercent: Double,
+    val distributionPercent: Int,
     val fundingEndDate: String,
     val graph: List<Graph>,
     val id: String,
     val image: String,
+    val marketEndDate: String,
     val name: String,
     val ownerId: String,
     val productQnA: List<QnA>,
+    val purchaseIsDisabled: Boolean,
+    val startAmount: Int,
     val targetAmount: Int,
     val tokenAddress: String,
     val type: String,
-    val userFavorite: Boolean,
-    val marketEndDate: String
+    val userFavorite: Boolean
 )
